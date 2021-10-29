16319b563c6a4c1dcdec3ae7899ff54f2065e4a7
robert morales <rmoralfit@gmail.com>
2021-10-29 15:55:12 -0400

    add member/members.js

---
+++ client/src/components/Member.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import NewMemberForm from './NewMemberForm'
+
+
+
+class Member extends Component {
+
+    render() {
+        return (
+            <div id={this.props.id}className="member has-text-weight-bold ">
+                <h2>{this.props.name}</h2>
+                <p>Name:{this.props.number}</p>
+                <p>AGE: {this.props.address}</p>
+                
+                <img className="member-img" width="100%" src={this.props.img_url} alt=" " />
+                    
+                    <div class="container">
+                    <div class="center">
+                <button id={this.props.id} class="button is-danger" onClick={this.props.deleteMember}>DELETE</button>
+                    </div>
+                </div>
+            
+            </div>
+
+        )
+    };
+}
+
+export default Member;
---
+++ client/src/components/Members.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import Member from './Member';
+import NewMemberForm from './NewMemberForm';
+import {Link} from 'react-router-dom';
+
+
+
+class Members extends React.Component {
+    state = {
+        members: []
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3000/members")
+            .then(res => res.json())
+            .then(data => this.setState({ members: data }),
+            console.log(this.state)
+            );
+    };
+
+    componentDidUpdate = () => {
+          fetch(`http://localhost:3000/members`)
+            .then((response) => response.json())
+            .then(data => {
+                if(data.length === this.state.members.length){
+                    console.log(data.length)
+                } else {
+                    this.setState({members: data})
+                }
+            })
+        }
+      
+
+    deleteMember = (e) => {
+        fetch(`http://localhost:3000/members/${e.target.id}`, {
+            method: "DELETE",
+          })
+        .then((response) => response.json())
+        .then((data) => {
+        this.setState({ members:[...this.state.members], data})
+            });
+        }
+
+    
+    gym = () => {
+        this.state.members.gym.map()
+     }
+
+    renderMembers = () => {
+        return (
+         <div>
+                {this.state.members.map(({id, name, number, address ,age, gym_id}) => 
+                    <Member 
+                        key={id}
+                        deleteMember={this.deleteMember}
+                        id={id}
+                        name={name}
+                        number={number}
+                        address={address}
+                        gym_id={gym_id}
+                    />
+                )}
+            </div>
+            
+        )
+    };
+
+    render() {
+        return (
+            
+            <>
+                
+                    <Link class="button is-info"to="/members/new">New Member</Link>
+                    <ul>{this.renderMembers()}</ul>
+            </>
+        )}
+    };
+
+
+export default Members;
