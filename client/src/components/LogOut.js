0c680f05c1547b5d4856726a7486763f9ffdd91c
robert morales <rmoralfit@gmail.com>
2021-10-29 19:34:49 -0400

    added logout

---
+++ client/src/components/LogOut.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+function Navbar({ onLogout }) {
+    function handleLogout() {
+      fetch("/logout", {
+        method: "DELETE",
+      }).then(() => onLogout());
+    }
+  
+    return (
+      <header>
+        <button onClick={handleLogout}>Logout</button>
+      </header>
+    );
+  } 
+
+  export default Navbar;
